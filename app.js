0f21e816e1547592d3115f24a89d1c701876e736
Ruby Srivastava <ruby.srivas1984@gmail.com>
2021-10-19 16:24:56 +0200

    Using types

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("test file!");
+function add(num1, num2) {
+    return num1 + num2;
+}
+var number1 = 10;
+var number2 = 7.5;
+var result = add(number1, number2);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,7 @@
-console.log("test file!");
+function add(num1:number, num2:number) {
+    return num1 + num2;
+}
+const number1 = 10;
+const number2 = 7.5;
+const result = add(number1,number2);
+console.log(result);
