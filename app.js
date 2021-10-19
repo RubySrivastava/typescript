d55c1a3c7d88f423aeb329af907acec12214cda5
Ruby Srivastava <ruby.srivas1984@gmail.com>
2021-10-20 00:14:22 +0200

    Use Array types

--- app.js
+++ app.js
@@ -4,6 +4,14 @@
 //} = {
 var person = {
     name: "Ruby",
-    age: 37
+    age: 37,
+    hobbies: ['Sports', 'Cooking']
 };
+var favoriteActivity;
+favoriteActivity = ['Sports'];
 console.log(person.name);
+console.log(favoriteActivity);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- app.ts
+++ app.ts
@@ -4,6 +4,14 @@
 //} = {
 const person = {
     name:"Ruby",
-    age: 37
+    age: 37,
+    hobbies: ['Sports','Cooking']
 }
+let favoriteActivity:string[]
+favoriteActivity = ['Sports']
 console.log(person.name)
+console.log(favoriteActivity)
+for (const hobby of person.hobbies){
+    console.log(hobby.toUpperCase())
+//    console.log(hobby.map()) !!Error!!
+}
