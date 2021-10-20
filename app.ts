1f3ac5dbe75d7e150c9335885c58bc69cf0a338a
Ruby Srivastava <ruby.srivas1984@gmail.com>
2021-10-20 10:42:02 +0200

    any type

--- app.ts
+++ app.ts
@@ -29,7 +29,7 @@ const person = {
 //person.role.push('admin')
 //person.role[1]=10 !!Error!!
 
-let favoriteActivity:string[]
+let favoriteActivity:any
 favoriteActivity = ['Sports']
 console.log(person.name)
 console.log(favoriteActivity)
