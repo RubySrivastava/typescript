a5a308fa8328d56ca571d64e1a22238c060bad3f
Ruby Srivastava <ruby.srivas1984@gmail.com>
2021-10-20 13:24:04 +0200

    never types

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-var userInput;
+var userInput; //unknown type
 var userName;
 userInput = 5;
 userInput = 'Max';
 if (typeof userInput === 'string') {
     userName = userInput;
 }
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError('An error occured', 500);
--- app.ts
+++ app.ts
@@ -7,3 +7,8 @@ userInput = 'Max';
 if (typeof userInput === 'string') {
     userName = userInput;
 }
+
+function generateError(message: string, code: number){
+    throw {message:message, errorCode:code}
+} // never type
+generateError('An error occured',500);
