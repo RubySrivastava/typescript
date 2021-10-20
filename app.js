4ba81354bca5e2c09c0f6c8a95f744226712ecb6
Ruby Srivastava <ruby.srivas1984@gmail.com>
2021-10-20 13:16:13 +0200

    unknown types

--- app.js
+++ app.js
@@ -1,19 +1,7 @@
-function add(num1, num2) {
-    return num1 + num2;
-} //number function types(return value)
-function printResult(num) {
-    console.log('Result is' + num);
-} //void function types(no return)
-function addAndHandle(num1, num2, cb) {
-    var result = num1 + num2;
-    cb(result);
-} // function types and callback
-var result = printResult(add(10, 5));
-//let someValue:undefined; !! undefined type
-var combineValues; //as function type
-combineValues = add;
-//combineValues=printResult  !!Error
-console.log(combineValues(10, 6));
-addAndHandle(10, 6, function (result) {
-    console.log(result);
-});
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
