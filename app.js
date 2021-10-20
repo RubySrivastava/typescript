954cbe63c4e95e374f875d1aca65f2c545881ded
Ruby Srivastava <ruby.srivas1984@gmail.com>
2021-10-20 10:37:25 +0200

    Enum types

--- app.js
+++ app.js
@@ -2,13 +2,34 @@
 //    name:string;
 //    age:number;
 //} = {
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string]
+// } = {
+//     name:"Ruby",
+//     age: 37,
+//     hobbies: ['Sports','Cooking'],
+//     role: [2,'Author']
+// }
+// const ADMIN = 0; !!JS!!
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: "Ruby",
     age: 37,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'Author']
+    role: Role.ADMIN
 };
-person.role.push('admin');
+//person.role.push('admin')
+//person.role[1]=10 !!Error!!
 var favoriteActivity;
 favoriteActivity = ['Sports'];
 console.log(person.name);
@@ -17,4 +38,7 @@ for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
     //    console.log(hobby.map()) !!Error!!
+}
+if (person.role === Role.ADMIN) {
+    console.log("IS ADMIN");
 }
--- app.ts
+++ app.ts
@@ -2,16 +2,28 @@
 //    name:string;
 //    age:number;
 //} = {
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string]
-} = {
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string]
+// } = {
+//     name:"Ruby",
+//     age: 37,
+//     hobbies: ['Sports','Cooking'],
+//     role: [2,'Author']
+// }
+
+// const ADMIN = 0; !!JS!!
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {ADMIN,READ_ONLY,AUTHOR}
+const person = {
     name:"Ruby",
     age: 37,
     hobbies: ['Sports','Cooking'],
-    role: [2,'Author']
+    role: Role.ADMIN
 }
 
 //person.role.push('admin')
@@ -24,4 +36,8 @@ console.log(favoriteActivity)
 for (const hobby of person.hobbies){
     console.log(hobby.toUpperCase())
 //    console.log(hobby.map()) !!Error!!
+}
+
+if (person.role===Role.ADMIN) {
+    console.log("IS ADMIN")
 }
