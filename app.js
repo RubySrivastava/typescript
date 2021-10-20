a83195778a1cdd2a2521edb83bb41e81037480b0
Ruby Srivastava <ruby.srivas1984@gmail.com>
2021-10-20 11:25:33 +0200

    Literal type

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-function combine(number1, number2) {
+function combine(number1, number2, resultConversion) {
     var result;
-    if (typeof number1 == 'number' && typeof number2 == 'number') {
-        result = number1 + number2;
+    if (typeof number1 === 'number' && typeof number2 === 'number' || resultConversion === 'as-number') {
+        result = +number1 + +number2;
     }
     else {
         result = number1.toString() + number2.toString();
     }
     return result;
 }
-var combinedAge = combine(10, 5);
+var combinedAge = combine(10, 5, 'as-number');
 console.log(combinedAge);
-var combinedName = combine("Max", "Life");
+var combinedStringAges = combine(10, 5, 'as-number');
+console.log(combinedStringAges);
+var combinedName = combine("Max", "Life", 'as-text');
 console.log(combinedName);
--- app.ts
+++ app.ts
@@ -1,14 +1,20 @@
-function combine (number1: number | string, number2: number | string) {
+function combine (
+    number1: number | string,
+    number2: number | string,
+    resultConversion:'as-number' | 'as-text') // string but used union types in literal types 
+    {
     let result;
-    if (typeof number1 == 'number' && typeof number2 == 'number') {
-        result = number1 + number2;
+    if (typeof number1 === 'number' && typeof number2 === 'number' || resultConversion === 'as-number') {
+        result = +number1 + +number2;
     } else {
         result = number1.toString() + number2.toString()
     }
     return result;
 }
 
-const combinedAge = combine(10,5)
+const combinedAge = combine(10,5,'as-number')
 console.log(combinedAge)
-const combinedName = combine("Max","Life")
+const combinedStringAges = combine(10,5,'as-number')
+console.log(combinedStringAges)
+const combinedName = combine("Max","Life",'as-text')
 console.log(combinedName)
