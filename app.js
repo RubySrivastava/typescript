60bddd902a0b9c1e4810a55223a43c2d4b1a78ec
Ruby Srivastava <ruby.srivas1984@gmail.com>
2021-10-20 12:56:36 +0200

    Fuction Types and callback

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 function add(num1, num2) {
     return num1 + num2;
-}
+} //number function types(return value)
 function printResult(num) {
     console.log('Result is' + num);
-}
+} //void function types(no return)
+function addAndHandle(num1, num2, cb) {
+    var result = num1 + num2;
+    cb(result);
+} // function types and callback
 var result = printResult(add(10, 5));
 //let someValue:undefined; !! undefined type
-var combineValues;
+var combineValues; //as function type
 combineValues = add;
 //combineValues=printResult  !!Error
 console.log(combineValues(10, 6));
+addAndHandle(10, 6, function (result) {
+    console.log(result);
+});
--- app.ts
+++ app.ts
@@ -1,9 +1,16 @@
-function add(num1:number, num2:number){ //number functio types(return value)
+function add(num1:number, num2:number){ 
     return num1 + num2
-}
-function printResult(num:number) {  //void function types(no return)
+} //number function types(return value)
+
+function printResult(num:number) {  
     console.log('Result is'+ num)
-}
+}  //void function types(no return)
+
+function addAndHandle(num1:number, num2:number, cb:(num:number) => void) {
+    const result = num1+num2;
+    cb(result)
+}  // function types and callback
+
 const result= printResult(add(10,5))
 
 //let someValue:undefined; !! undefined type
@@ -12,3 +19,7 @@ let combineValues:(num1:number,num2:number) => number  //as function type
 combineValues = add
 //combineValues=printResult  !!Error
 console.log(combineValues(10,6))
+
+addAndHandle(10,6,result => {
+    console.log(result);
+})
